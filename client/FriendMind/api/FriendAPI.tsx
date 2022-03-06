32f63a48d4f61ffc850b28c1326220a46959f3a3
tim <tpatrickdev@gmail.com>
2022-03-06 14:34:45 +0000

    connect to back end api to get and post friends

---
+++ client/FriendMind/api/FriendAPI.tsx
@@ -0,0 +1,80 @@
+/* 
+1. Fetch All Friends from server.
+2. Fetch the communication preferences.
+3. For each communication preference
+  a) find the last time 
+  b) create new friend with info
+*/
+
+/* 
+  The format I need to end up with is 'Friend'
+  UserId defaults to 1 for now
+*/
+
+const baseUrl = 'http://10.0.2.2:3000';
+
+export const getFriends = async (userId: number) => {
+  return fetch(`${baseUrl}/friends?id=${userId}`)
+      .then(friends => friends.json())
+} 
+
+export const addFriend = async (userId: number, friend: FriendForAdd, preferences: Array<CommPreference>) => {
+ 
+  console.log('====================================');
+  console.log('friend', friend);
+  console.log('====================================');
+  return fetch(`${baseUrl}/friend?id=${userId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ friend, preferences })
+  }).then(resp => resp.json());
+}
+
+export type FriendForAdd = {
+  firstName: string,
+  lastName: string,
+  birthDay: number,
+  birthMonth: string
+}
+
+export type Friend = {
+  id: number,
+  firstName: string,
+  lastName: string, 
+  birthDay: number,
+  birthMonth: string,
+  lastComms: Array<LastComm>,
+  notes?: Array<string>
+}
+
+export type FriendWithLastComms = {
+  firstName: string,
+  lastName: string, 
+  lastComms: Array<LastComm>,
+}
+
+export type Reminder = {
+  friendId: number,
+  firstName: string,
+  lastName: string,
+  lastComm: LastComm
+}
+export type LastComm = {
+  preference: CommPreference, 
+  lastCommunication: Date
+}
+
+export type CommPreference = {
+  mode: 'Write' | 'Talk' | 'Meet' | 'Added'
+  timeUnit: 'Days' | 'Weeks' | 'Months' | 'Years';
+  amount: number;
+}
+
+export type FriendForCard = {
+  firstName: string,
+  lastName: string;
+  lastSeen: Date;
+}
+
